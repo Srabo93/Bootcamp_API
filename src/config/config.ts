26420dcf80065f6cbab177df88b1d2894e9c008f
Srabo93 <a.s-dev@web.de>
2023-03-26 17:32:44 +0200

    adjusted Dockerfile paths, added volume to compose for .env files

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,15 +1,18 @@
 import * as dotenv from "dotenv";
 import fs from "fs";
 
-if (fs.existsSync("../../.env")) {
+dotenv.config();
+if (fs.existsSync(__dirname + "../.env")) {
   console.log("Using .env file to supply config environment variables");
-  dotenv.config({ path: "../../.env" });
+  dotenv.config({ path: __dirname + "../.env" });
 } else {
   console.log("Using .env.example file to supply config environment variables");
-  dotenv.config({ path: "../../.env" }); // you can delete this after you create your own .env file!
+  dotenv.config({ path: ".env.example" }); // you can delete this after you create your own .env file!
 }
-
-export const MONGODB_URI = "mongodb://mongo:27017/Bootcamp" as string;
+if (fs.existsSync(__dirname + "../test.txt")) {
+  console.log("ja gibts");
+}
+export const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
   console.log("No client secret. Set MONGODB_URI environment variable.");
