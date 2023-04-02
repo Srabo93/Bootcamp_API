560393fc921750724bb96d60b55431df0a587a9b
Srabo93 <a.s-dev@web.de>
2023-04-02 13:21:16 +0200

    added types and added envs

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,6 +11,8 @@ if (fs.existsSync(`${__dirname}/.env`)) {
 
 export const MONGODB_URI = process.env.MONGODB_URI as string;
 export const GEOCODER_API_KEY = process.env.GEOCODER_API_KEY as string;
+export const JWT_EXPIRE = process.env.JWT_EXPIRE as string;
+export const JWT_SECRET = process.env.JWT_SECRET as string;
 
 if (!MONGODB_URI) {
   console.log("No client secret. Set MONGODB_URI environment variable.");
@@ -19,5 +21,15 @@ if (!MONGODB_URI) {
 
 if (!GEOCODER_API_KEY) {
   console.log("No client secret. Set GEOCODER_API_KEY environment variable.");
+  process.exit(1);
+}
+
+if (!JWT_EXPIRE) {
+  console.log("No client secret. Set JWT_EXPIRE environment variable.");
+  process.exit(1);
+}
+
+if (!JWT_SECRET) {
+  console.log("No client secret. Set JWT_SECRET environment variable.");
   process.exit(1);
 }
