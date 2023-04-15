661c8c4679d5faf82bbe133687888eb7de996555
Srabo93 <a.s-dev@web.de>
2023-04-15 23:40:44 +0200

    added trim functionality to the schemas

--- src/utils/zodSchemas.ts
+++ src/utils/zodSchemas.ts
@@ -4,16 +4,16 @@ export const registerSchema = z.object({
   body: z.object({
     name: z.string({
       required_error: "Name is required",
-    }).max(256),
+    }).trim().max(256),
     email: z.string({
       required_error: "Email is required",
-    }).max(256).email("Not a valid email"),
+    }).trim().max(256).email("Not a valid email"),
     password: z.string({
       required_error: "Password is required",
-    }).max(256).min(6),
+    }).trim().max(256).min(6),
     role: z.string({
       required_error: "Role is required",
-    }).max(256),
+    }).trim().max(256),
   }),
 });
 
@@ -21,23 +21,23 @@ export const loginScheme = z.object({
   body: z.object({
     email: z.string({
       required_error: "Email is required",
-    }).max(256).email("Not a valid email"),
+    }).trim().max(256).email("Not a valid email"),
     password: z.string({
       required_error: "Password is required",
-    }).max(256).min(6),
+    }).trim().max(256).min(6),
   }),
 });
 
 export const currentUserScheme = z.object({
   user: z.object({
-    id: z.string({ required_error: "User id not provided" }).max(256),
+    id: z.string({ required_error: "User id not provided" }).trim().max(256),
   }),
 });
 
 export const updateUserDetailsScheme = z.object({
   body: z.object({
-    name: z.string().max(256).optional(),
-    email: z.string().max(256).email().optional(),
+    name: z.string().trim().max(256).optional(),
+    email: z.string().trim().max(256).email().optional(),
   }),
 });
 
@@ -45,14 +45,14 @@ export const updatePasswordScheme = z.object({
   body: z.object({
     currentPassword: z.string({
       required_error: "Current password is required",
-    }).max(256).min(6),
+    }).trim().max(256).min(6),
     newPassword: z.string({
       required_error: "New password is required",
-    }).max(256).min(6),
+    }).trim().max(256).min(6),
   }),
   req: z.object({
     user: z.object({
-      id: z.string({ required_error: "No user id provided" }),
+      id: z.string({ required_error: "No user id provided" }).trim()
     }),
   }),
 });
@@ -61,6 +61,6 @@ export const forgotPasswordScheme = z.object({
   body: z.object({
     email: z.string({
       required_error: "Email is required",
-    }).max(256).email("Not a valid email"),
+    }).trim().max(256).email("Not a valid email"),
   }),
 });
