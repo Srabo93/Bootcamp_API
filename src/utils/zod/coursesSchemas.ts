b1067d62056d8afb2f1be6aba0e5f178ec5279e0
Srabo93 <a.s-dev@web.de>
2023-04-18 21:40:28 +0200

    added reviewSchema and adjusted updateschema

--- src/utils/zod/coursesSchemas.ts
+++ src/utils/zod/coursesSchemas.ts
@@ -2,46 +2,58 @@ import { z } from "zod";
 
 export const byIdCourseScheme = z.object({
   params: z.object({
-    id: z.string({
-      required_error: "Course id is required",
-    }).trim().max(256),
+    id: z
+      .string({
+        required_error: "Course id is required",
+      })
+      .trim()
+      .max(256),
   }),
 });
 
 export const addCourseScheme = z.object({
   body: z.object({
-    title: z.string({ required_error: "Course Name required" }).trim().max(
-      256,
-    ),
-    description: z.string({ required_error: "Course description required" })
-      .trim().max(256),
+    title: z.string({ required_error: "Course Name required" }).trim().max(256),
+    description: z
+      .string({ required_error: "Course description required" })
+      .trim()
+      .max(256),
     weeks: z.number().max(20).min(1),
     tuition: z.number().max(40000).min(1000),
-    minimumSkill: z.string({ required_error: "Minimum Skill is required" })
-      .trim().max(256),
+    minimumSkill: z
+      .string({ required_error: "Minimum Skill is required" })
+      .trim()
+      .max(256),
     scholarshipAvailable: z.boolean(),
   }),
 });
 
 export const updateCourseScheme = z.object({
-  params: z.object({
-    bootcampId: z.string({ required_error: "Bootcamp id not provided" }),
-  }),
   user: z.object({
     id: z.string({ required_error: "User id required" }).trim().max(256),
   }),
-  body: z.object({
-    title: z.string({ required_error: "Course Name required" }).trim().max(
-      256,
-    ),
-    description: z.string({ required_error: "Course description required" })
-      .trim().max(256),
-    weeks: z.number().max(20).min(1),
-    tuition: z.number().max(40000).min(1000),
-    minimumSkill: z.string({ required_error: "Minimum Skill is required" })
-      .trim().max(256),
-    scholarshipAvailable: z.boolean(),
-  }).optional(),
+  body: z
+    .object({
+      title: z
+        .string({ required_error: "Course Name required" })
+        .trim()
+        .max(256)
+        .optional(),
+      description: z
+        .string({ required_error: "Course description required" })
+        .trim()
+        .max(256)
+        .optional(),
+      weeks: z.number().max(20).min(1).optional(),
+      tuition: z.number().max(40000).min(1000).optional(),
+      minimumSkill: z
+        .string({ required_error: "Minimum Skill is required" })
+        .trim()
+        .max(256)
+        .optional(),
+      scholarshipAvailable: z.boolean().optional(),
+    })
+    .optional(),
 });
 
 export const deleteCourseScheme = z.object({
